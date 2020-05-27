b571ac1ca68b0511b29df1c8e8e5498ad173f511
= <=>
2020-05-27 14:33:42 +0700

    add Screen ManageUser

---
+++ app/face_checkin_app_mobile/screens/ManageUser.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, StatusBar} from 'react-native';
+
+export default class ManageUser extends React.Component {
+    render() {
+        return (
+            <View>
+                <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#0a4ff0" translucent = {true}/>
+                <Text>aaaaaa</Text>
+            </View>
+        )
+    }
+}
