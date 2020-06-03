ebb020819bb4f432531adc55bd1f6bdb608cdb70
Nghia Pham <nghia.pq77@gmail.com>
2020-06-03 09:58:56 +0700

    Add relative path for config

--- service/config.py
+++ service/config.py
@@ -1,6 +1,10 @@
+import sys
+import os
+PATH = sys.path[0]
+
 config = {
-    'PATH_ARCFACE_MODEL': '/Users/haophanthanh/Work/BK/Mobile/face-checkin-app-mobile/service/core/models/arcface.pth' ,
-    'PATH_DB_MODEL': 'sqlite:////Users/haophanthanh/Work/BK/Mobile/face-checkin-app-mobile/service/database.db',
+    'PATH_ARCFACE_MODEL': os.path.join(PATH, 'core/models/arcface.pth'),
+    'PATH_DB_MODEL': 'sqlite:///' + os.path.join(PATH, 'database.db'),
     'SECRET_KEY': 'Fac3Chexkin1@39&74^3ApM1N11@3',
     'TOKEN_TIME_LIFE': 1000,
     'PATH_DATA_FOLDERS': 'core/data',
