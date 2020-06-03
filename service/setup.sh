b65c16c940de92fcb2879e1d97caec48769dc2f6
haophan <hao.phanthanh98@gmail.com>
2020-06-03 09:25:43 +0700

    fix settup file for update pip

--- service/setup.sh
+++ service/setup.sh
@@ -4,11 +4,13 @@ if [ ! -d "venv" ]; then
 	echo "Setup environment"
 	python3 -m venv venv
 	source venv/bin/activate
+	python3 -m pip install pip --upgrade
 	python3 -m pip install -r requirements.txt
 fi
 if [ -f "venv/bin/activate" ]; then
 	echo "Loading Python vitualenvl from folder venv"
 	source venv/bin/activate
+	python3 -m pp install pip --upgrade
 else
 	echo "Folder venv is error, please check it!"
 fi
