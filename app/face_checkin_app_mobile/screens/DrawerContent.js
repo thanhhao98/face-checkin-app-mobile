9ac223cf6afe688c2e53250d4bd6fea499b8d69a
= <=>
2020-06-03 12:27:17 +0700

    change menu

--- app/face_checkin_app_mobile/screens/DrawerContent.js
+++ app/face_checkin_app_mobile/screens/DrawerContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, AsyncStorage } from 'react-native'
 import { 
     Avatar,
     Title,
@@ -16,8 +16,61 @@ import {
 } from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+
 export function DrawerContent(props){
+  console.log(props.isLogin,'islogin')
+  
+  if(!props.isLogin){
     return(
+      <View style={{flex:1}}>
+          <DrawerContentScrollView {...props}>
+              <View styles = {styles.drawerContent}>
+                  <View style = {styles.userInfoSection}>
+                    <View style = {{ marginTop:15}}>
+                      <Avatar.Image 
+                        source ={{
+                          uri:'http://streaming1.danviet.vn/upload/2-2018/images/2018-06-21/anh-1-1529571305-width650height1018.jpg',
+                        }}
+                        size = {50}
+                      />
+                    </View>
+                    <View style ={{marginLeft:15, marginTop:15}}>
+                      <Title style={styles.title}>Mai Van Hao</Title>
+                      <Caption style={styles.caption}>maihao1997@gmail.com</Caption>
+                    </View>
+                  </View>
+                  
+                  <Drawer.Section style={styles.drawerSection}>
+                    <DrawerItem 
+                      icon={({color, size})=>(
+                        <Icon name="camera"
+                        color={color}
+                        size={size}
+                        />
+                      )}
+                      label="Checkin"
+                      onPress={()=>{props.navigation.navigate('Checkin')}}
+                    />
+                    <DrawerItem 
+                      icon={({color, size})=>(
+                        <Icon name="sign-in"
+                        color={color}
+                        size={size}
+                        />
+                      )}
+                      label="Login"
+                      onPress={()=>{props.navigation.navigate('Login')}}
+                    />
+                  </Drawer.Section>
+              </View>
+          </DrawerContentScrollView>
+          
+              
+      </View>
+  )
+  }else {
+    if(props.isAdmin){
+      return(
         <View style={{flex:1}}>
             <DrawerContentScrollView {...props}>
                 <View styles = {styles.drawerContent}>
@@ -49,16 +102,6 @@ export function DrawerContent(props){
                       />
                       <DrawerItem 
                         icon={({color, size})=>(
-                          <Icon name="sign-in"
-                          color={color}
-                          size={size}
-                          />
-                        )}
-                        label="Login"
-                        onPress={()=>{props.navigation.navigate('Login')}}
-                      />
-                      <DrawerItem 
-                        icon={({color, size})=>(
                           <Icon name="history"
                           color={color}
                           size={size}
@@ -82,26 +125,77 @@ export function DrawerContent(props){
                         onPress={() => props.navigation.openDrawer()}
                       /> */}
                     </Drawer.Section>
-
-                    
                     
                    
                 </View>
             </DrawerContentScrollView>
+                
             <Drawer.Section style={styles.bottomDrawerSection}>
-                <DrawerItem 
-                  icon={({color, size})=>(
-                    <Icon name="sign-out"
-                    color={color}
-                    size={size}
-                    />
-                  )}
-                  label="Log Out"
-                  onPress={()=>{}}
-                />
-            </Drawer.Section>
+              <DrawerItem 
+                icon={({color, size})=>(
+                  <Icon name="sign-out"
+                  color={color}
+                  size={size}
+                  />
+                )}
+                label='Logout'
+                onPress={()=>{}}
+              />
+          </Drawer.Section>
         </View>
-    )
+      )
+    } else{
+      return(
+        <View style={{flex:1}}>
+            <DrawerContentScrollView {...props}>
+                <View styles = {styles.drawerContent}>
+                    <View style = {styles.userInfoSection}>
+                      <View style = {{ marginTop:15}}>
+                        <Avatar.Image 
+                          source ={{
+                            uri:'http://streaming1.danviet.vn/upload/2-2018/images/2018-06-21/anh-1-1529571305-width650height1018.jpg',
+                          }}
+                          size = {50}
+                        />
+                      </View>
+                      <View style ={{marginLeft:15, marginTop:15}}>
+                        <Title style={styles.title}>Mai Van Hao</Title>
+                        <Caption style={styles.caption}>maihao1997@gmail.com</Caption>
+                      </View>
+                    </View>
+                    
+                    <Drawer.Section style={styles.drawerSection}>
+                      <DrawerItem 
+                        icon={({color, size})=>(
+                          <Icon name="camera"
+                          color={color}
+                          size={size}
+                          />
+                        )}
+                        label="Checkin"
+                        onPress={()=>{props.navigation.navigate('Checkin')}}
+                      />
+                      <DrawerItem 
+                        icon={({color, size})=>(
+                          <Icon name="history"
+                          color={color}
+                          size={size}
+                          />
+                        )}
+                        label="History"
+                        onPress={()=>{props.navigation.navigate('History')}}
+                      />
+                    </Drawer.Section>
+                    
+                   
+                </View>
+            </DrawerContentScrollView>
+        </View>
+      )
+    }
+    
+  }
+    
 }
 const styles = StyleSheet.create({
     drawerContent: {
