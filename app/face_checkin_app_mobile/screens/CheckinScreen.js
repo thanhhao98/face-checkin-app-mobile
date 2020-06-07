5c03394d9dd15ff54c32a8394b0aa9bf571fcd09
haophan <hao.phanthanh98@gmail.com>
2020-06-07 23:44:14 +0700

    fix error server when no face, and handel on client

--- app/face_checkin_app_mobile/screens/CheckinScreen.js
+++ app/face_checkin_app_mobile/screens/CheckinScreen.js
@@ -6,134 +6,155 @@ import {
 	Text,
 	StatusBar,
 	Image,
-	Animated
+	Animated,
+	Dimensions,
+	TouchableHighlight
 } from 'react-native';
 import {RNCamera}  from 'react-native-camera';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { max } from 'react-native-reanimated';
-import { IconButton } from 'react-native-paper';
-import { ProgressBar, Colors  } from 'react-native-paper';
+import {IconButton} from 'react-native-paper';
+import { ProgressBar} from 'react-native-paper';
 
 export default class CheckinScreen extends React.Component {
   constructor() {
     super();
     this.state={
+			username: '',
+			status: '',
 			progressBar: false,
       image:'',
-			cameraIsOn: true,
+			cameraOn: true,
     }
   }
-	toggleCamera() {
-		this.setState({
-			cameraIsOn: !this.state.cameraIsOn
-		})
-	}
   render() {
     return (
-      <View style={styles.container}>
+			<View style={styles.container}>
 				<StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#0a4ff0" translucent = {true}/>
-				{this.state.cameraIsOn ? (
-	        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-						style={[styles.preview, { transform: [{rotateY: '180deg'}]  }]}
-          type={RNCamera.Constants.Type.front}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
+				{this.state.cameraOn ? (
+					<RNCamera
+					ref={ref => {
+						this.camera = ref;
+						}}
+						style={[styles.preview, { transform: [{rotateY: '180deg'}]   }]}
+						type={RNCamera.Constants.Type.front}
+						androidCameraPermissionOptions={{
+							title: 'Permission to use camera',
+							message: 'We need your permission to use your camera',
+							buttonPositive: 'Ok',
+							buttonNegative: 'Cancel',
+						}}
 					/>
-				): (
-          <Image
+				):(
+					<Image
 						style={styles.preview}
-            source={{
-              uri: this.state.image,
-            }}
-          /> 
+						source={{
+							uri: this.state.image,
+						}}
+					/> 
 				)}
 				<View>
-					<ProgressBar indeterminate={true} progress={0.5} visible={this.state.progressBar}/>
+					<ProgressBar indeterminate={true} visible={this.state.progressBar}/>
 				</View>
-				        
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity 
-            onPress={this.takePicture.bind(this)} style={styles.capture}>
-          <Icon
-            name="camera"
-            size={40}
-            style={{ color:"white", textAlign:"center", marginTop:5 }}
-          />
-          </TouchableOpacity>
-        </View>
-        <View style={{ flexDirection:'row', justifyContent:"space-between" }}>
-          <View>
-            <Text>Hello Mai Van Hao</Text>
-            <Text>Checkin Success</Text>
-            <Text>Time chekcin: 8:00/1111</Text>
-          </View>
-    
-        </View>
-      </View>
-    );
+				<View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center'  }}>
+					<TouchableOpacity
+						onPress={this.takePicture.bind(this)} style={styles.capture}
+					>
+						<Icon
+							name="camera"
+							size={40}
+							style={{ color:"white", textAlign:"center", marginTop:5  }}
+						>
+						</Icon>
+					</TouchableOpacity>
+				</View>
+				<View>
+					<Text
+						style={{textAlign: 'center'}}
+					>
+						{this.state.username}
+					</Text>
+					<Text
+						style={{textAlign: 'center'}}
+					>
+						{this.state.status}
+					</Text>
+				</View>
+			</View>
+		);
   }
-
   takePicture = async () => {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true};
-      const data = await this.camera.takePictureAsync(options);
-			this.setState({progressBar: true})
-      this.setState({image:data.uri})
-			this.toggleCamera()
-			console.log('ok')
-			let res = await fetch("http://192.168.20.81:5000/api/v1/checkFace", {
+		if (this.camera) {
+			this.setState({
+				username: '',
+				status: 'Checking',
+				progressBar: true,
+			})
+			while(true){
+				const options = { quality: 0.5, base64: true};
+				const data = await this.camera.takePictureAsync(options);
+				let res = await fetch("http://192.168.0.148:5000/api/v1/checkFace", {
 				method: 'POST',
-        headers: {
+				headers: {
 					'Accept': 'application/json',
 					'Content-Type': 'application/json',
-        },
+				},
 				body: JSON.stringify({
-					'data': data,
+						'data': data,
+					})
 				})
-			})
-			console.log('ok1')
-			let response = await res.json();
-			console.log('ok2')
-			console.log(response)
-			this.setState({progressBar: false})
-			console.log('done')
+				let response = await res.json()
+				if (response['status']){
+					this.setState({
+						username: response['data']['username'],
+						status: '',
+						progressBar: false,
+						cameraOn: false,
+						image: data.uri,
+					})
+					break
+				}
+				else{
+					this.setState({
+						username: '',
+						status: response['message']
+					})
+				}
+			}
 		} else {
-			this.setState({progressBar: false})
-			this.toggleCamera()
+			console.log('setcamera')
+			this.setState({
+				username: '',
+				status: '',
+				progressBar: false,
+				cameraOn: true,
+			})
 		}
-  };
+	}		
+};
 
-}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-  },
-  preview: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    height: 400,
-    overflow: "hidden"
-  },
-  capture: {
-    flex: 0,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-    width:80, 
-    height: 80, 
-    borderRadius:40, 
-    backgroundColor:'#0a4ff0',
-    opacity:0.3
-  }
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		backgroundColor: '#fff',
+		  
+	},
+	preview: {
+		flexDirection: 'column',
+		alignItems: 'center',
+		height: 400,
+		overflow: "hidden"
+	},
+	capture: {
+		flex: 0,
+		padding: 15,
+		paddingHorizontal: 20,
+		alignSelf: 'center',
+		margin: 20,
+		width:80, 
+		height: 80, 
+		borderRadius:40, 
+		backgroundColor:'#0a4ff0',
+		opacity:0.3
+	}
 });
