0b3504cf0b34192f93892800374b9b1631f8644c
= <=>
2020-06-12 02:08:56 +0700

    add component Header

---
+++ app/face_checkin_app_mobile/components/header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { IconButton } from 'react-native-paper';
+const statusbarHeight = StatusBar.currentHeight;
+
+export default function Header ({navigation,title, isStatus}) {
+    return (
+            <View style={styles.header}>
+                <Icon.Button 
+                    name="ios-menu" 
+                    size= {28}
+                    style={styles.icon}
+                    onPress={()=>navigation.openDrawer()}
+                />
+                <Text style={styles.headerText}>{title}</Text>
+                {isStatus? <Icon.Button
+                    name="ios-add"
+                    size={28}
+                    style={styles.icon2}
+                    onPress={()=>navigation.navigate('AddUser')}
+                /> : null}
+                
+            </View>
+        )
+}
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: '8%',
+        top:statusbarHeight,
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor:'#4179f7',
+    },
+    headerText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: "white",
+        letterSpacing: 1,
+        
+    },
+    icon: {
+        paddingLeft:20,
+        backgroundColor: "#4179f7"
+    },
+    icon2: {
+        // position:'absolute',
+        // left:-20,
+        backgroundColor: "#4197f7"
+    }
+})
