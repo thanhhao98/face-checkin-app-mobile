22da2e633bce4a816f5bd643bbf4ed9f313b854e
= <=>
2020-06-12 04:48:41 +0700

    add AddUserScreen

--- app/face_checkin_app_mobile/screens/AddUserScreen.js
+++ app/face_checkin_app_mobile/screens/AddUserScreen.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import { StyleSheet, View, Text, TextInput, StatusBar, AsyncStorage,  } from 'react-native';
+import { StyleSheet, View, Text, TextInput, StatusBar, AsyncStorage } from 'react-native';
 import { Title, Button } from 'react-native-paper';
-// import ImagePicker from 'react-native-image-picker';
+
 
 class AddUserScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             username: '',
-            password: '',
             email: '',
-            photo: null,
+            password: '',
+            checkLogin: false,
         }
     }
     sendData = async (props) => {
+        let token = await AsyncStorage.getItem('token') || false;
         try {
             let data = {
                 username: this.state.username,
+                email: this.state.email,
                 password: this.state.password
             }
             let post = { data }
-            let res = await fetch('https://cca354e1fd3d.ngrok.io/api/v1/login', {
+            let res = await fetch('http://192.168.2.18:5000/api/v1/createUser', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
+                    'x-access-token': token,
                 },
                 body: JSON.stringify(post)
             });
@@ -35,23 +38,34 @@ class AddUserScreen extends React.Component {
                     checkLogin: true
                 })
             }
-            else {
-                let isAdmin = response.data.isAdmin;
-                let token = response.data.token;
-                try {
-                    await AsyncStorage.setItem('isAdmin', isAdmin.toString());
-                    await AsyncStorage.setItem('isLogin', 'true');
-                    await AsyncStorage.setItem('token', token.toString())
-                    this.props.navigation.navigate('History')
-                } catch (error) {
-                    console.error(error.message)
-                }
-            }
+//             else {
+//                 let isAdmin = response.data.isAdmin;
+//                 let token = response.data.token;
+//                 try {
+//                     await AsyncStorage.setItem('isAdmin', isAdmin.toString());
+//                     await AsyncStorage.setItem('isLogin', 'true');
+//                     await AsyncStorage.setItem('token', token.toString())
+// 0
+//                     //get data for manage history screen
+//                     let res = await fetch("http://192.168.0.46:5000/api/v1/getCheckHistory", {
+//                     method: 'GET',
+//                     headers: {
+//                         'Accept': 'application/json',
+//                         'Content-Type': 'application/json',
+//                         'x-access-token': token,
+//                     }});
+//                     let response = await res.json();
+//                     console.log(response)
+//                     // this.props.navigation.navigate('History',{data:response})
+//                 } catch (error) {
+//                     console.error(error.message)
+//                 }
+//             }
 
             this.setState({
                 username: '',
-                password: '',
                 email: '',
+                password: ''
             })
             // AsyncStorage.setItem('aaaaa','asdfasdfg7h9832j0p')
         } catch (e) {
@@ -62,51 +76,20 @@ class AddUserScreen extends React.Component {
         console.error(e)
     }
 
-    chooseFile = () => {
-        // var options = {
-        //   title: 'Select Image',
-        //   customButtons: [
-        //     { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
-        //   ],
-        //   storageOptions: {
-        //     skipBackup: true,
-        //     path: 'images',
-        //   },
-        // };
-        // ImagePicker.showImagePicker(options, response => {
-        //   console.log('Response = ', response);
-    
-        //   if (response.didCancel) {
-        //     console.log('User cancelled image picker');
-        //   } else if (response.error) {
-        //     console.log('ImagePicker Error: ', response.error);
-        //   } else if (response.customButton) {
-        //     console.log('User tapped custom button: ', response.customButton);
-        //     alert(response.customButton);
-        //   } else {
-        //     let source = response;
-        //     // You can also display the image using data:
-        //     // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-        //     this.setState({
-        //       filePath: source,
-        //     });
-        //   }
-        // });
-      };
     render() {
-        const { photo } = this.state
         return (
             <View style={styles.container}>
 
                 <StatusBar barStyle="dark-content" hidden={false} backgroundColor="#0a4ff0" translucent={true} />
                 <View style={styles.down}>
-                    
+                    <Title style={{ color: 'blue' }} >Login to your account</Title>
                     {/* <Paragraph> Hi there! Nice to see you again.</Paragraph> */}
                     <View style={styles.textInputContainer}>
-                        <Text style={{ paddingLeft: 3, color: 'red' }}>username</Text>
+                        <Text style={{ paddingLeft: 3, color: 'red' }}>Username</Text>
                         <TextInput
                             style={styles.textInput}
-                            placeholder='Enter your email'
+                            placeholder='Enter your username'
+                            textContentType='emailAddress'
                             keyboardType='email-address'
                             underlineColorAndroid={
                                 "black"
@@ -115,24 +98,6 @@ class AddUserScreen extends React.Component {
                             onChangeText={(text) => this.setState({ username: text,  checkLogin: false })}
                         />
                         {this.state.checkLogin ? <Text style={{color:'#FF7F50',paddingLeft:3,fontStyle: 'italic'}}>wrong username or password</Text> : null}
-                    </View>
-                    <View style={styles.textInputContainer}>
-                        <Text style={{ paddingLeft: 3, color: 'red' }}>Password</Text>
-                        <View sytle={{ flexDirection: 'row' }}>
-                            <TextInput
-                                style={styles.textInput}
-                                placeholder='Enter your password'
-                                secureTextEntry={this.state.secureTextEntry}
-                                underlineColorAndroid={
-                                    "black"
-                                }
-                                value={this.state.password}
-                                onChangeText={(text) => this.setState({ password: text, checkLogin: false })}
-                            >
-                            </TextInput>
-                            {this.state.checkLogin ? <Text style={{color:'#FF7F50',paddingLeft:3,fontStyle: 'italic'}}>wrong username or password</Text> : null}
-                            
-                        </View>
                     </View>
                     <View style={styles.textInputContainer}>
                         <Text style={{ paddingLeft: 3, color: 'red' }}>Email</Text>
@@ -145,30 +110,31 @@ class AddUserScreen extends React.Component {
                                 "black"
                             }
                             value={this.state.email}
-                            onChangeText={(text) => this.setState({ username: text,  checkLogin: false })}
+                            onChangeText={(text) => this.setState({ email: text,  checkLogin: false })}
                         />
                         {this.state.checkLogin ? <Text style={{color:'#FF7F50',paddingLeft:3,fontStyle: 'italic'}}>wrong username or password</Text> : null}
                     </View>
-                    <View style={styles.container}>
-                        {/*<Image 
-                        source={{ uri: this.state.filePath.path}} 
-                        style={{width: 100, height: 100}} />*/}
-                        <Image
-                            source={{
-                            uri: 'data:image/jpeg;base64,' + this.state.filePath.data,
-                            }}
-                            style={{ width: 100, height: 100 }}
-                        />
-                        <Image
-                            source={{ uri: this.state.filePath.uri }}
-                            style={{ width: 250, height: 250 }}
-                        />
-                        <Text style={{ alignItems: 'center' }}>
-                            {this.state.filePath.uri}
-                        </Text>
-                        <Button title="Choose File" onPress={this.chooseFile.bind(this)} />
+                    <View style={styles.textInputContainer}>
+                        <Text style={{ paddingLeft: 3, color: 'red' }}>Password</Text>
+                        <View sytle={{ flexDirection: 'row' }}>
+                            <TextInput
+                                style={styles.textInput}
+                                placeholder='Enter your password'
+                                secureTextEntry={true}
+                                underlineColorAndroid={
+                                    "black"
+                                }
+                                value={this.state.password}
+                                onChangeText={(text) => this.setState({ password: text, checkLogin: false })}
+                            >
+                            </TextInput>
+                            {this.state.checkLogin ? <Text style={{color:'#FF7F50',paddingLeft:3,fontStyle: 'italic'}}>wrong username or password</Text> : null}
+                            
+                        </View>
                     </View>
-                    <Button style={styles.buttonSignIn} onPress={this.sendData} >Sign In</Button>
+                    
+                    <Button style={styles.buttonSignIn} onPress={this.sendData} >Sign Up</Button>
+
                 </View>
             </View>
         );
