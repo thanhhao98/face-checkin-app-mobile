2b0235282a5ea1924cda1743d4c3d8a588e2d2f3
haophan <hao.phanthanh98@gmail.com>
2020-06-14 09:43:23 +0700

    fix fking err

--- app/face_checkin_app_mobile/android/app/src/main/java/com/face_checkin_app_mobile/MainApplication.java
+++ app/face_checkin_app_mobile/android/app/src/main/java/com/face_checkin_app_mobile/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
